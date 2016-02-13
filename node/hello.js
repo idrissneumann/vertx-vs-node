51c8c9dba43c706f6505f7a2cf38ba5d3cf4412b
ineumann <idriss.neumann@docapost.fr>
2016-02-13 13:25:30 +0100

    Adding node hello

---
+++ node/hello.js
@@ -0,0 +1,14 @@
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
+  response.end("<html><body><h1>Trop cool ce BBL !</h1></body></html>\n");
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
